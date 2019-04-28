576dbe58a8d3938d1bca632202b887e2b56e545a
Krzysztof Chrusciel <krzysztof.chrusciel@jamf.com>
2019-04-28 11:00:33 +0200

    Add icons

---
+++ src/pl/codecouple/kubernetes/browser/presentation/KubernetesBrowserToolWindowFactory.java
@@ -0,0 +1,16 @@
+package pl.codecouple.kubernetes.browser.presentation;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+
+public class KubernetesBrowserToolWindowFactory implements ToolWindowFactory {
+
+	@Override
+	public void createToolWindowContent(@NotNull final Project project, @NotNull final ToolWindow toolWindow) {
+
+	}
+
+}
